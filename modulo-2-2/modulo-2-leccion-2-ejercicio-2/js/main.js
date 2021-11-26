56bcd0247e9a8bca76360141dadc941b38292f9e
sarapalcam <sara.pal.cam@gmail.com>
2021-11-26 11:16:16 +0100

    add exercises 2-3 module 2 lesson 2

---
+++ modulo-2-2/modulo-2-leccion-2-ejercicio-2/js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const superWave = document.querySelector('.super-wave');
+
+superWave.classList.add('important');
+
+console.log(superWave);
---
+++ modulo-2-2/modulo-2-leccion-2-ejercicio-3/js/main.js
@@ -0,0 +1,5 @@
+const priceKiwi = 5;
+const pricePear = 2;
+const priceGrapes = 4;
+
+console.log(priceKiwi * 2 + pricePear * 3 + priceGrapes * .5);
