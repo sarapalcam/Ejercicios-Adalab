b0f734819b05c0308c3fe0f98a9a01be9ec8b6be
saraPalCam <sara.pal.cam@gmail.com>
2021-12-06 18:52:24 +0100

    add changes exercise 4 lesson 2.7

--- modulo-2-7/modulo-2-leccion-7-ejercicio-4/js/main.js
+++ modulo-2-7/modulo-2-leccion-7-ejercicio-4/js/main.js
@@ -8,3 +8,13 @@ Cada 3 años
 ¿Próximas 15 lunas?
 
 */
+
+
+ let initDate = Date.parse(2017, 9, 5);
+ let endDate = Date.parse(2032, 11, 31);
+ let calcYear = (endDate - initDate) / 15; 
+ let threeYears = calcYear * 3;
+
+for (let i = initDate; i < endDate; i = i + threeYears) {
+     console.log(`La próxima Luna del Cazador será el: ${i}`);
+}
