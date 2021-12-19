409f3a4f69577fc9896a302b8abeaa5092fb309d
sarapalcam <sara.pal.cam@gmail.com>
2021-12-19 12:55:29 +0100

    add changes bmk

--- modulo-2/modulo-2-ejecicio-bookmarks/js/main.js
+++ modulo-2/modulo-2-ejecicio-bookmarks/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Lista de variables globales: selectores de HTML
 const menuDropdown = document.querySelector(".js_menu_dropdown");
 const linkDropdown = document.querySelector(".js_link_dropdown");
 const sectionAdd = document.querySelector(".js_section_add");
@@ -8,7 +9,12 @@ const boardData = document.querySelector(".js_boarddata");
 const listData = document.querySelector(".js_list_data");
 const buttonShowCardview = document.querySelector(".js_cardview_button");
 const buttonShowTable = document.querySelector(".js_table_button");
+const saveBtn = document.querySelector(".js_save_btn");
 
+//Variable en la que meteremos toda la información de los bookmarks al pintarla
+let html = "";
+
+//Lista de los bookmarks disponibles
 const bmkData = [
   {
     url: "https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion",
@@ -30,6 +36,7 @@ const bmkData = [
   },
 ];
 
+//Función que nos pinta las etiquetas de los bookmars
 function renderTags(tags) {
   let htmlTags = `<ul class="item__tags">`;
   for (const tag of tags) {
@@ -38,6 +45,7 @@ function renderTags(tags) {
   return (htmlTags += `</ul>`);
 }
 
+//Función que nos recoge en una vriable los datos de los bookmars
 function renderBookmark(bmkData) {
   const htmlTags = renderTags(bmkData.tags);
   const htmlBookmark = `
@@ -59,14 +67,15 @@ function renderBookmark(bmkData) {
   return htmlBookmark;
 }
 
-let html = "";
-
+//Bucle que nos introduce en la variable HTML los datos de los bookmars
 for (const bookmark of bmkData) {
   html += renderBookmark(bookmark);
 }
 
+//Pintamos los datos de los bookmarks
 listData.innerHTML = html;
 
+//Funciones para cambiar la vista de los resultados a tabla o tarjetas
 function setCardListView() {
   boardData.classList.remove("tableview");
   boardData.classList.add("listview");
@@ -77,6 +86,7 @@ function setTableView() {
   boardData.classList.add("tableview");
 }
 
+//Funciones que nos muestran o nos ocultan el menú hamburgesa
 function showDropDownMenu() {
   menuDropdown.classList.remove("collapsed");
 }
@@ -93,11 +103,12 @@ function toggleDropDownMenu() {
   }
 }
 
+//FUNCIONES MANEJADORAS DE EVENTOS
+
+//Función que nos muestra el formulario para crear un nuevo bookmark
 function showAddForm() {
   sectionAdd.classList.remove("hidden");
 }
-
-linkDropdown.addEventListener("click", handleClickLinkDropdown);
 
 function handleClickLinkDropdown(event) {
   event.preventDefault();
@@ -106,7 +117,6 @@ function handleClickLinkDropdown(event) {
 
 function handleClickShowCardview(event) {
   event.preventDefault();
-
   setCardListView();
   buttonShowTable.classList.remove("selected");
   buttonShowCardview.classList.add("selected");
@@ -114,13 +124,52 @@ function handleClickShowCardview(event) {
 
 function handleClickShowTable(event) {
   event.preventDefault();
-
   setTableView();
   buttonShowCardview.classList.remove("selected");
   buttonShowTable.classList.add("selected");
 }
 
+// Esta función obtiene los valores de cada uno de los inputs, crea un nuevo objeto newBookmarkDataObject que agregaremos al listado de enlaces con la siguiente línea de código:
+
+// bmkData.push(newBookmarkDataObject);
+// Recuerda limpiar los valores de los inputs y volver a ocultar la sección del formulario.
+
+const searchInputs = document.querySelectorAll(".js_search_input");
+
+const newBookmarkDataObject = {
+  url: "",
+  desc: "",
+  tags: [],
+};
+
+function addNewBmk() {
+  bmkData.push(newBookmarkDataObject);
+  // renderBookmark(bmkData);
+  listData.innerHTML += html;
+}
+
+function getInputsSearch() {
+  for (const input of searchInputs) {
+    if (input.name === "new_dir") {
+      newBookmarkDataObject.url = input.value;
+    } else if (input.name === "new_desc") {
+      newBookmarkDataObject.desc = input.value;
+    } else if (input.name === "new_tags") {
+      newBookmarkDataObject.tags.push(input.value);
+    }
+  }
+  addNewBmk();
+}
+//NO FUNCIONA
+
+function saveNewBookmark(event) {
+  event.preventDefault();
+  getInputsSearch();
+}
+
+//Eventos
+linkDropdown.addEventListener("click", handleClickLinkDropdown);
 buttonShowCardview.addEventListener("click", handleClickShowCardview);
 buttonShowTable.addEventListener("click", handleClickShowTable);
-
 buttonAdd.addEventListener("click", showAddForm);
+saveBtn.addEventListener("click", saveNewBookmark);
