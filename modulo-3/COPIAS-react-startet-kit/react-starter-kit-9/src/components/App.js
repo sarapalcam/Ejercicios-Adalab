c908ce7c549f6e477b87fd72d4cc3165d66f2aee
sarapalcam <sara.pal.cam@gmail.com>
2022-01-11 18:42:02 +0100

    add react kit copies

---
+++ modulo-3/COPIAS-react-startet-kit/react-starter-kit-9/src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/index.scss';
+import '../styles//App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
